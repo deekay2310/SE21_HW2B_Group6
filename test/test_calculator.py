9a2442f63e81d4fd5a3eade7cd8344e3f9665d70
Dev Kumar <devskumar99@gmail.com>
2021-09-01 20:59:46 -0400

    Create test_calculator.py
    Added test cases for basic arithmetic operation

---
+++ test/test_calculator.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append("../Calculator")
+from Calculator import addition, subtraction, multiplication, division
+
+def test_addition():
+    assert addition(3,4) == 7.0
+    
+def test_subtraction():
+    assert subtraction(3,4) == -1.0
+
+def test_multiplication():
+    assert multiplication(3,4) == 12.0
+
+def test_division():
+    assert division(3,4) == 0.75
+    
+test_addition()
+test_subtraction()
+test_multiplication()
+test_division()
