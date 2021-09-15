c267cf55fb3a183233b94c1ca21c41d70ad896e5
radhikaraman20 <89432698+radhikaraman20@users.noreply.github.com>
2021-09-15 00:09:18 -0400

    Update Calculator.py

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -21,7 +21,7 @@ def division(a,b):
 
 def nth_root(a,n):
     print('root({}) to the power {} = {}'.format(a,n,a**n))
-    return float(a)^(1.0/float(n))
+    return a**(1.0/n)
 
 def logarithms(a,base):
           print('log({}) with base {} = {}'.format(a,base,log(a,base)))
