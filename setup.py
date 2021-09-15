d9e3bb70e110fb66277e7406fdebf57a62b55256
radhikaraman20 <89432698+radhikaraman20@users.noreply.github.com>
2021-09-14 23:19:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name="Calculator", version="3.0.1",description="A simple calculator for basic mathematical operations", author="Dev, Prakruthi, Radhika, Rohan, Sunidhi",packages=['Calculator'])
+setup(
+    name='Calculator',
+    version='1.0.0',
+    packages=find_packages(),
+    license='none',
+    description='A simple calculator for basic mathematical operations',
+    author='Dev, Prakruthi, Radhika, Rohan, Sunidhi',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
