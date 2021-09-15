345409d3a87d49e07c2e6dd2d6353b8d933fa7ba
radhikaraman20 <89432698+radhikaraman20@users.noreply.github.com>
2021-09-15 00:13:09 -0400

    Update test_root_log.py

--- test/test_root_log.py
+++ test/test_root_log.py
@@ -1,10 +1,10 @@
 #import sys
 import pytest
 #sys.path.append("../Calculator")
-from Calculator.Calculator import nth_root, logarithms
+from Calculator.Calculator import nth_power, logarithms
 
 def test_nth_root():
-    assert nth_root(3,2) == 9.0
+    assert nth_power(3,2) == 9.0
     
 def test_logarithm():
     assert logarithms(2,2) == 1.0
