0b2dfb53a3261866c69bc2cab0af392253455df8
radhikaraman20 <89432698+radhikaraman20@users.noreply.github.com>
2021-09-14 23:55:34 -0400

    Update test_root_log.py

--- test/test_root_log.py
+++ test/test_root_log.py
@@ -7,7 +7,7 @@ def test_nth_root():
     assert nth_root(3,2) == 9.0
     
 def test_logarithm():
-    assert logarithm(2,2) == 1.0
+    assert logarithms(2,2) == 1.0
 
 #test_nth_root() 
 #test_logarithm()
