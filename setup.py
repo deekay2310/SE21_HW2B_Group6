af75765108e831fc91ec82df380d75106634c4de
Dev Kumar <devskumar99@gmail.com>
2021-09-14 23:08:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name="Calculator", version="3.0",description="A simple calculator for basic mathematical operations", author="Dev, Prakruthi, Radhika, Rohan, Sunidhi",packages=['Calculator'])
+setup(name="Calculator", version="3.0.1",description="A simple calculator for basic mathematical operations", author="Dev, Prakruthi, Radhika, Rohan, Sunidhi",packages=['Calculator'])
